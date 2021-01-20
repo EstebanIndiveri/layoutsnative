b8684a264f902eac8d0715076ef877b884287f16
Esteban Indiveri <esteban.indiveri@gmail.com>
2021-01-19 22:15:47 -0300

    add base work

---
+++ App.js
@@ -0,0 +1,118 @@
+import React, { Fragment } from 'react';
+import {
+  StyleSheet,
+  View,
+  Image,
+  StatusBar,
+  Text,
+  ScrollView,
+} from 'react-native';
+
+const App = () => {
+  return (
+    <Fragment>
+    <StatusBar translucent backgroundColor="transparent"/>
+      <ScrollView>
+        <View style={{flexDirection:'row'}}>
+          <Image
+            style={styles.banner}
+            source={require('./assets/img/bg.jpg')}
+          />
+        </View>
+      
+      <View>
+        <View style={styles.contenedor}>
+          <Text style={styles.titulo}>Qué hacer en París</Text>
+        
+        <ScrollView
+        horizontal
+        >
+          <View>
+            <Image
+              style={styles.ciudad}
+              source={require('./assets/img/actividad1.jpg')}
+            />
+          </View>
+          <View>
+            <Image
+              style={styles.ciudad}
+              source={require('./assets/img/actividad2.jpg')}
+            />
+          </View>
+          <View>
+            <Image
+              style={styles.ciudad}
+              source={require('./assets/img/actividad3.jpg')}
+            />
+          </View>
+          <View>
+            <Image
+              style={styles.ciudad}
+              source={require('./assets/img/actividad4.jpg')}
+            />
+          </View>
+          <View>
+            <Image
+              style={styles.ciudad}
+              source={require('./assets/img/actividad5.jpg')}
+            />
+          </View>
+        </ScrollView>
+        <Text style={styles.titulo}>Los mejores alojamientos</Text>
+          <View>
+            <View>
+              <Image
+                style={styles.mejores}
+                source={require('./assets/img/mejores1.jpg')}
+              />
+            </View>
+            <View>
+              <Image
+                style={styles.mejores}
+                source={require('./assets/img/mejores2.jpg')}
+              />
+            </View>
+            <View>
+              <Image
+                style={styles.mejores}
+                source={require('./assets/img/mejores3.jpg')}
+              />
+            </View>
+          </View>
+      </View>
+      </View>
+      </ScrollView>
+    </Fragment>
+  );
+};
+
+const styles = StyleSheet.create({
+  banner:{
+    height:250,
+    flex:1,
+  },
+  titulo:{
+    fontWeight:'bold',
+    fontSize:24,
+    marginVertical:20,
+  },
+  contenedor:{
+    marginHorizontal:10,
+  },
+  ciudad:{
+    width:250,
+    height:300,
+    marginRight:10,
+    borderRadius:15
+  },
+  mejores:{
+    width:'100%',
+    height:200,
+    marginVertical:5,
+    borderRadius:15,
+ 
+    
+  }
+});
+
+export default App;
